8f400a4650dcd20ba000391f663d2cc211a1e81e
Max Cyprys <mc.cyprysik@icloud.com>
2022-03-10 20:17:44 +0100

    <3

---
+++ a.py
@@ -0,0 +1,18 @@
+def get_scale(pkt):
+    final = []
+    arr = [['cel', pkt, pkt], ['bdb', pkt * 88 / 100, pkt * 99 / 100], ['db', pkt * 72 / 100, pkt * 87 / 100],
+           ['dst', pkt * 56 / 100, pkt * 71 / 100], ['dop', pkt * 41 / 100, pkt * 55 / 100],
+           ['ndst', pkt * 0 / 100, pkt * 40 / 100]]
+    for i in arr:
+        final.append([i[0], round(i[1],1), round(i[2],1)])
+    return final
+
+
+'''
+ocena celująca – 100%;
+ocena bardzo dobra – od 88% do 99%;
+ocena dobra – od 72% do 87%;
+ocena dostateczna – od 56% do 71%;
+ocena dopuszczająca – od 41% do 55%;
+ocena niedostateczna – 0% do 40%;
+'''
---
+++ app.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, render_template
+from a import get_scale
+app = Flask(__name__)
+
+
+@app.route('/', methods=["GET", "POST"])
+def punctation():
+    if request.method == "POST":
+        point = request.form.get("pkt2")
+        if point == "":
+            point = 10
+        try:
+            int(point)
+        except:
+            return render_template("form.html")
+        x = get_scale(int(point))
+        return render_template("final.html", arr=x)
+    return render_template("form.html")
+
+
+if __name__ == '__main__':
+    app.run()
+'''
+ocena celująca – 100%;
+ocena bardzo dobra – od 88% do 99%;
+ocena dobra – od 72% do 87%;
+ocena dostateczna – od 56% do 71%;
+ocena dopuszczająca – od 41% do 55%;
+ocena niedostateczna – 0% do 40%;
+'''
