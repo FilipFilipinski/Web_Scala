091d7984713b54730d78a56f721afcbf4015d747
Max Cyprys <mc.cyprysik@icloud.com>
2022-03-24 11:52:14 +0100

    bug-fixing(Error info.html -> Info.html)

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/info')
 def info():
-    return render_template("info.html")
+    return render_template("Info.html")
 
 
 @app.route('/', methods=["GET", "POST"])
